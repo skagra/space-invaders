d81a3cc94fc12164afef83fd3bf67d5e0d27b9f4
Paul Harvey <paul@pauls-place.me.uk>
2024-04-21 19:07:27 +0100

    Added initial animation

--- game/main.asm
+++ game/main.asm
@@ -26,8 +26,7 @@ main:
     POP HL
 
     ; Draw some aliens
-    
-    LD C, 10
+    LD C, 20
 cloop:
     LD B, 50
     LD HL, aliens
@@ -39,49 +38,64 @@ aloop:
     LD DE, sprite_blank
     PUSH DE
 
+    ; Wait for screen blank
     HALT
 
     CALL draw.draw_sprite
     POP DE
     POP DE
 
-    LD DE, (HL) ; Coords
-    INC DE      ; Down one row
+    ; Move to new position
+    LD DE, (HL)     ; Coords
+    INC DE          ; Down one row
     LD (HL), DE  
-
-    PUSH DE     ; New coords
+    PUSH DE         ; New coords
     
-    INC HL      ; Skip to sprite data
+    INC HL          ; Skip to sprite data
     INC HL
-    LD DE, (HL) ; Sprite
+
+    LD A, C         ; Loop counter
+    AND 0x01        ; Even?
+    JR Z, even
+    INC HL          ; Second variant of sprite
+    INC HL
+even:    
+    LD DE, (HL)     ; Sprite
     PUSH DE
 
     INC HL
     INC HL
 
+    LD A, C         ; Loop counter
+    AND 0x01        ; Even?
+    JR NZ, odd
+    INC HL
+    INC HL
+odd:
     CALL draw.draw_sprite
     POP DE
     POP DE
-    DEC B
+    DEC B           ; One more alien has been done
     JR NZ,aloop
 
-    DEC C
+    DEC C           ; Number of draw cycles
     JR NZ,cloop
 forever: JP forever
 
-aliens:         WORD 0x1060,sprite_a_1_0, 0x2060,sprite_a_1_0, 0x3060,sprite_a_1_0, 0x4060,sprite_a_1_0, 0x5060,sprite_a_1_0
-                WORD 0x6060,sprite_a_1_0, 0x7060,sprite_a_1_0, 0x8060,sprite_a_1_0, 0x9060,sprite_a_1_0, 0xA060,sprite_a_1_0
+aliens:         WORD 0x1060,sprite_a_1_0,sprite_a_1_1, 0x2060,sprite_a_1_0,sprite_a_1_1, 0x3060,sprite_a_1_0,sprite_a_1_1, 0x4060,sprite_a_1_0,sprite_a_1_1, 0x5060,sprite_a_1_0,sprite_a_1_1
+                WORD 0x6060,sprite_a_1_0,sprite_a_1_1, 0x7060,sprite_a_1_0,sprite_a_1_1, 0x8060,sprite_a_1_0,sprite_a_1_1, 0x9060,sprite_a_1_0,sprite_a_1_1, 0xA060,sprite_a_1_0,sprite_a_1_1
 
-                WORD 0x1050,sprite_a_1_0, 0x2050,sprite_a_1_0, 0x3050,sprite_a_1_0, 0x4050,sprite_a_1_0, 0x5050,sprite_a_1_0
-                WORD 0x6050,sprite_a_1_0, 0x7050,sprite_a_1_0, 0x8050,sprite_a_1_0, 0x9050,sprite_a_1_0, 0xA050,sprite_a_1_0
+                WORD 0x1050,sprite_a_1_0,sprite_a_1_1, 0x2050,sprite_a_1_0,sprite_a_1_1, 0x3050,sprite_a_1_0,sprite_a_1_1, 0x4050,sprite_a_1_0,sprite_a_1_1, 0x5050,sprite_a_1_0,sprite_a_1_1
+                WORD 0x6050,sprite_a_1_0,sprite_a_1_1, 0x7050,sprite_a_1_0,sprite_a_1_1, 0x8050,sprite_a_1_0,sprite_a_1_1, 0x9050,sprite_a_1_0,sprite_a_1_1, 0xA050,sprite_a_1_0,sprite_a_1_1
 
-                WORD 0x1040,sprite_a_2_0, 0x2040,sprite_a_2_0, 0x3040,sprite_a_2_0, 0x4040,sprite_a_2_0, 0x5040,sprite_a_2_0
-                WORD 0x6040,sprite_a_2_0, 0x7040,sprite_a_2_0, 0x8040,sprite_a_2_0, 0x9040,sprite_a_2_0, 0xA040,sprite_a_2_0
-                WORD 0x1030,sprite_a_2_0, 0x2030,sprite_a_2_0, 0x3030,sprite_a_2_0, 0x4030,sprite_a_2_0, 0x5030,sprite_a_2_0
-                WORD 0x6030,sprite_a_2_0, 0x7030,sprite_a_2_0, 0x8030,sprite_a_2_0, 0x9030,sprite_a_2_0, 0xA030,sprite_a_2_0
+                WORD 0x1040,sprite_a_2_0,sprite_a_2_1, 0x2040,sprite_a_2_0,sprite_a_2_1, 0x3040,sprite_a_2_0,sprite_a_2_1, 0x4040,sprite_a_2_0,sprite_a_2_1, 0x5040,sprite_a_2_0,sprite_a_2_1
+                WORD 0x6040,sprite_a_2_0,sprite_a_2_1, 0x7040,sprite_a_2_0,sprite_a_2_1, 0x8040,sprite_a_2_0,sprite_a_2_1, 0x9040,sprite_a_2_0,sprite_a_2_1, 0xA040,sprite_a_2_0,sprite_a_2_1
 
-                WORD 0x1020,sprite_a_3_0, 0x2020,sprite_a_3_0, 0x3020,sprite_a_3_0, 0x4020,sprite_a_3_0, 0x5020,sprite_a_3_0
-                WORD 0x6020,sprite_a_3_0, 0x7020,sprite_a_3_0, 0x8020,sprite_a_3_0, 0x9020,sprite_a_3_0, 0xA020,sprite_a_3_0
+                WORD 0x1030,sprite_a_2_0,sprite_a_2_1, 0x2030,sprite_a_2_0,sprite_a_2_1, 0x3030,sprite_a_2_0,sprite_a_2_1, 0x4030,sprite_a_2_0,sprite_a_2_1, 0x5030,sprite_a_2_0,sprite_a_2_1
+                WORD 0x6030,sprite_a_2_0,sprite_a_2_1, 0x7030,sprite_a_2_0,sprite_a_2_1, 0x8030,sprite_a_2_0,sprite_a_2_1, 0x9030,sprite_a_2_0,sprite_a_2_1, 0xA030,sprite_a_2_0,sprite_a_2_1
+
+                WORD 0x1020,sprite_a_3_0,sprite_a_3_1, 0x2020,sprite_a_3_0,sprite_a_3_1, 0x3020,sprite_a_3_0,sprite_a_3_1, 0x4020,sprite_a_3_0,sprite_a_3_1, 0x5020,sprite_a_3_0,sprite_a_3_1
+                WORD 0x6020,sprite_a_3_0,sprite_a_3_1, 0x7020,sprite_a_3_0,sprite_a_3_1, 0x8020,sprite_a_3_0,sprite_a_3_1, 0x9020,sprite_a_3_0,sprite_a_3_1, 0xA020,sprite_a_3_0,sprite_a_3_1
 
 sprite_blank:   BYTE 0x02, 0x08
                 BYTE 0b00000000, 0b00000000
@@ -91,7 +105,9 @@ sprite_blank:   BYTE 0x02, 0x08
                 BYTE 0b00000000, 0b00000000
                 BYTE 0b00000000, 0b00000000
                 BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000    
+                BYTE 0b00000000, 0b00000000   
+
+; First alien 
 sprite_a_1_0:   BYTE 0x02, 0x08
                 BYTE 0b00000011, 0b11000000
                 BYTE 0b00011111, 0b11111000
@@ -102,6 +118,17 @@ sprite_a_1_0:   BYTE 0x02, 0x08
                 BYTE 0b00001101, 0b10110000
                 BYTE 0b00110000, 0b00001100
 
+sprite_a_1_1:   BYTE 0x02, 0x08
+                BYTE 0b00000011, 0b11000000
+                BYTE 0b00011111, 0b11111000
+                BYTE 0b00111111, 0b11111100
+                BYTE 0b00111001, 0b10011100
+                BYTE 0b00111111, 0b11111100
+                BYTE 0b00001110, 0b01110000
+                BYTE 0b00011001, 0b10011000
+                BYTE 0b00001100, 0b00110000
+
+; Second alien
 sprite_a_2_0:   BYTE 0x02, 0x08
                 BYTE 0b00000100, 0b00010000
                 BYTE 0b00010010, 0b00100100
@@ -112,7 +139,17 @@ sprite_a_2_0:   BYTE 0x02, 0x08
                 BYTE 0b00000100, 0b00010000
                 BYTE 0b00001000, 0b00001000
 
+sprite_a_2_1:   BYTE 0x02, 0x08
+                BYTE 0b00000100, 0b00010000
+                BYTE 0b00000010, 0b00100000
+                BYTE 0b00000111, 0b11110000
+                BYTE 0b00001101, 0b11011000
+                BYTE 0b00011111, 0b11111100
+                BYTE 0b00010111, 0b11110100
+                BYTE 0b00010100, 0b00010100
+                BYTE 0b00000011, 0b01100000
 
+; Third alien
 sprite_a_3_0:   BYTE 0x02, 0x08
                 BYTE 0b00000001, 0b10000000
                 BYTE 0b00000011, 0b11000000
@@ -122,6 +159,16 @@ sprite_a_3_0:   BYTE 0x02, 0x08
                 BYTE 0b00000010, 0b01000000
                 BYTE 0b00000101, 0b10100000
                 BYTE 0b00001010, 0b01010000
+
+sprite_a_3_1:   BYTE 0x02, 0x08
+                BYTE 0b00000001, 0b10000000
+                BYTE 0b00000011, 0b11000000
+                BYTE 0b00000111, 0b11100000
+                BYTE 0b00001101, 0b10110000
+                BYTE 0b00001111, 0b11110000
+                BYTE 0b00000101, 0b10100000
+                BYTE 0b00001000, 0b00010000
+                BYTE 0b00000100, 0b00100000
 
 
 ; Put the stack immediated after the code
