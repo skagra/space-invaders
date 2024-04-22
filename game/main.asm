279fba27b97397de9b465db0571054ce56cb1e1c
Paul Harvey <paul@pauls-place.me.uk>
2024-04-22 07:51:46 +0100

    Some code tidying

--- game/main.asm
+++ game/main.asm
@@ -8,8 +8,8 @@
     ORG 0x0
     BLOCK mmap.FREE_MEMORY_START
 
-    include "utils.asm"
-    include "draw.asm"
+    INCLUDE "utils.asm"
+    INCLUDE "draw.asm"
     
     MODULE main
 
@@ -27,60 +27,60 @@ main:
 
     ; Draw some aliens
     LD C, 20
-cloop:
-    LD B, 50
-    LD HL, aliens
-aloop:
-    LD DE, (HL) ; Coords
-    PUSH DE
+animation_loop:
+    LD B,50
+    LD HL,aliens
 
-    ; Blank old sprite
-    LD DE, sprite_blank
-    PUSH DE
-
+draw_pack_loop:
     ; Wait for screen blank
     HALT
 
+    ; Blank old sprite
+    LD DE,(HL)             ; Coords of current alien
+    PUSH DE     
+    LD DE,sprite_blank     ; Background square
+    PUSH DE
     CALL draw.draw_sprite
     POP DE
     POP DE
 
-    ; Move to new position
-    LD DE, (HL)     ; Coords
-    INC DE          ; Down one row
-    LD (HL), DE  
-    PUSH DE         ; New coords
+    ; Move sprite to new position
+    LD DE,(HL)              ; Coords
+    INC DE                  ; Down one row
+    LD (HL),DE              ; Overwrite old coords with new
+    PUSH DE                 ; New coords
     
-    INC HL          ; Skip to sprite data
+    INC HL                  ; Skip to pointer to sprite data
     INC HL
 
-    LD A, C         ; Loop counter
-    AND 0x01        ; Even?
-    JR Z, even
-    INC HL          ; Second variant of sprite
+    BIT 0x01,C              ; Which variant of sprite to draw
+    JR Z,variant_2
+    INC HL                  ; Skip to second variant of sprite
     INC HL
-even:    
-    LD DE, (HL)     ; Sprite
+variant_2:    
+    LD DE,(HL)              ; Sprite data pointer of alien
     PUSH DE
 
-    INC HL
+    INC HL                  ; Skip past the sprite pointer
     INC HL
 
-    LD A, C         ; Loop counter
-    AND 0x01        ; Even?
-    JR NZ, odd
+    BIT 0x01,C              ; If we selected the first variant then skip past the second variant pointer
+    JR NZ,variant_1
     INC HL
     INC HL
-odd:
-    CALL draw.draw_sprite
+variant_1:
+    CALL draw.draw_sprite   ; Draw the alien sprite
+    POP DE                  ; Remove parameters from stack
     POP DE
-    POP DE
-    DEC B           ; One more alien has been done
-    JR NZ,aloop
+    DEC B                   ; One more alien has been done, dec loop counter
+    JR NZ,draw_pack_loop    ; Done drawing sheet of aliens?
 
-    DEC C           ; Number of draw cycles
-    JR NZ,cloop
+    DEC C                   ; One more cycle of animating pack done
+    JR NZ,animation_loop    ; Done animating?
+
 forever: JP forever
+
+    INCLUDE "sprite_data.asm"
 
 aliens:         WORD 0x1060,sprite_a_1_0,sprite_a_1_1, 0x2060,sprite_a_1_0,sprite_a_1_1, 0x3060,sprite_a_1_0,sprite_a_1_1, 0x4060,sprite_a_1_0,sprite_a_1_1, 0x5060,sprite_a_1_0,sprite_a_1_1
                 WORD 0x6060,sprite_a_1_0,sprite_a_1_1, 0x7060,sprite_a_1_0,sprite_a_1_1, 0x8060,sprite_a_1_0,sprite_a_1_1, 0x9060,sprite_a_1_0,sprite_a_1_1, 0xA060,sprite_a_1_0,sprite_a_1_1
@@ -96,80 +96,6 @@ aliens:         WORD 0x1060,sprite_a_1_0,sprite_a_1_1, 0x2060,sprite_a_1_0,sprit
 
                 WORD 0x1020,sprite_a_3_0,sprite_a_3_1, 0x2020,sprite_a_3_0,sprite_a_3_1, 0x3020,sprite_a_3_0,sprite_a_3_1, 0x4020,sprite_a_3_0,sprite_a_3_1, 0x5020,sprite_a_3_0,sprite_a_3_1
                 WORD 0x6020,sprite_a_3_0,sprite_a_3_1, 0x7020,sprite_a_3_0,sprite_a_3_1, 0x8020,sprite_a_3_0,sprite_a_3_1, 0x9020,sprite_a_3_0,sprite_a_3_1, 0xA020,sprite_a_3_0,sprite_a_3_1
-
-sprite_blank:   BYTE 0x02, 0x08
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000
-                BYTE 0b00000000, 0b00000000   
-
-; First alien 
-sprite_a_1_0:   BYTE 0x02, 0x08
-                BYTE 0b00000011, 0b11000000
-                BYTE 0b00011111, 0b11111000
-                BYTE 0b00111111, 0b11111100
-                BYTE 0b00111001, 0b10011100
-                BYTE 0b00111111, 0b11111100
-                BYTE 0b00000110, 0b01100000
-                BYTE 0b00001101, 0b10110000
-                BYTE 0b00110000, 0b00001100
-
-sprite_a_1_1:   BYTE 0x02, 0x08
-                BYTE 0b00000011, 0b11000000
-                BYTE 0b00011111, 0b11111000
-                BYTE 0b00111111, 0b11111100
-                BYTE 0b00111001, 0b10011100
-                BYTE 0b00111111, 0b11111100
-                BYTE 0b00001110, 0b01110000
-                BYTE 0b00011001, 0b10011000
-                BYTE 0b00001100, 0b00110000
-
-; Second alien
-sprite_a_2_0:   BYTE 0x02, 0x08
-                BYTE 0b00000100, 0b00010000
-                BYTE 0b00010010, 0b00100100
-                BYTE 0b00010111, 0b11110100
-                BYTE 0b00011101, 0b11011100
-                BYTE 0b00011111, 0b11111100
-                BYTE 0b00001111, 0b11111000
-                BYTE 0b00000100, 0b00010000
-                BYTE 0b00001000, 0b00001000
-
-sprite_a_2_1:   BYTE 0x02, 0x08
-                BYTE 0b00000100, 0b00010000
-                BYTE 0b00000010, 0b00100000
-                BYTE 0b00000111, 0b11110000
-                BYTE 0b00001101, 0b11011000
-                BYTE 0b00011111, 0b11111100
-                BYTE 0b00010111, 0b11110100
-                BYTE 0b00010100, 0b00010100
-                BYTE 0b00000011, 0b01100000
-
-; Third alien
-sprite_a_3_0:   BYTE 0x02, 0x08
-                BYTE 0b00000001, 0b10000000
-                BYTE 0b00000011, 0b11000000
-                BYTE 0b00000111, 0b11100000
-                BYTE 0b00001101, 0b10110000
-                BYTE 0b00001111, 0b11110000
-                BYTE 0b00000010, 0b01000000
-                BYTE 0b00000101, 0b10100000
-                BYTE 0b00001010, 0b01010000
-
-sprite_a_3_1:   BYTE 0x02, 0x08
-                BYTE 0b00000001, 0b10000000
-                BYTE 0b00000011, 0b11000000
-                BYTE 0b00000111, 0b11100000
-                BYTE 0b00001101, 0b10110000
-                BYTE 0b00001111, 0b11110000
-                BYTE 0b00000101, 0b10100000
-                BYTE 0b00001000, 0b00010000
-                BYTE 0b00000100, 0b00100000
-
 
 ; Put the stack immediated after the code
 ; This seems to be needed so the debugger knows where the stack is
