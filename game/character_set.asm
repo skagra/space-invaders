97f6c2b68770f9b12d1325961cb6c8a372c9d57b
Paul Harvey <paul@pauls-place.me.uk>
2024-04-29 08:26:34 +0100

    Added some more sprites and start of character set

---
+++ game/character_set.asm
@@ -0,0 +1,14 @@
+    MODULE character_set
+
+CHARACTER_SET_BASE:
+CHAR_A:     BYTE 0b00000000
+            BYTE 0b00010000
+            BYTE 0b00101000
+            BYTE 0b01000100
+            BYTE 0b01000100
+            BYTE 0b01111100
+            BYTE 0b01000100
+            BYTE 0b01000100
+
+    ENDMODULE
+
