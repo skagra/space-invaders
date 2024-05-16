41c7a6429cf73744fe6f7a36e8c554396ae1eb83
Paul Harvey <paul@pauls-place.me.uk>
2024-05-16 09:43:13 +0100

    Added WPMEM fence for both draw stacks

--- game/fast_draw/fast_draw.asm
+++ game/fast_draw/fast_draw.asm
@@ -2,6 +2,7 @@
 ; since it was last flushed.
 _draw_buffer_stack_top: BLOCK 2                         ; Points to the next free location on the draw stack
 _DRAW_BUFFER_STACK:     BLOCK 512                       ; Size of the draw stack
+_BUFFER_STACK_FENCE:    BLOCK 2                            ; WPMEM,2   
 
 init:
     PUSH HL
