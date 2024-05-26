df9bfc1b9b9105109ae98dd8669951259be59a92
Paul Harvey <paul@pauls-place.me.uk>
2024-05-26 21:42:37 +0100

    Fixed formatting

--- game/global_state/update.asm
+++ game/global_state/update.asm
@@ -2,7 +2,7 @@ update:
     PUSH AF
 
     LD A,(_game_state)                                  ; Grab the current game state
-    BIT _GAME_STATE_ALIEN_EXPLODING_BIT,A                ; Is an alien exploding?
+    BIT _GAME_STATE_ALIEN_EXPLODING_BIT,A               ; Is an alien exploding?
     JR NZ,.state_alien_exploding                        ; Y - handle it
     
     JR .done                                            ; N - all done
@@ -17,7 +17,7 @@ update:
     CALL aliens.event_alien_explosion_done              ; Inform the alien pack  
     CALL player_missile.event_alien_explosion_done      ; Inform the player missile
     
-    LD A,_GAME_STATE_RUNNING_VALUE                             ; Update global state to nominal running
+    LD A,_GAME_STATE_RUNNING_VALUE                      ; Update global state to nominal running
     LD (_game_state),A
 
 .done
