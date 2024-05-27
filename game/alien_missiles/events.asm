8a3f193e247c92cdaf18a9e4c357cb24221efbe0
Paul Harvey <paul@pauls-place.me.uk>
2024-05-27 18:17:52 +0100

    Macro'd repeated code

--- game/alien_missiles/events.asm
+++ game/alien_missiles/events.asm
@@ -48,7 +48,7 @@ event_missiles_collided:
 
 .PARAM_ALIEN_MISSILE: EQU 10
 
-    PUSH AF,HL,IX,IY
+    PUSH HL,IX,IY
 
     LD  IX,0                                                ; Point IX to the stack
     ADD IX,SP  
@@ -58,9 +58,31 @@ event_missiles_collided:
     
     LD (IY+_ALIEN_MISSILE_OFFSET_STATE),_ALIEN_MISSILE_STATE_MISSILES_COLLIDED_VALUE 
 
-    POP IY,IX,HL,AF
+    POP IY,IX,HL
 
     RET
+
+    MACRO REMOVE_ALIENT_MISSILE num
+
+        LD IX,_ALIEN_MISSILE_num
+        BIT _ALIEN_MISSILE_STATE_NOT_ACTIVE_BIT,(IX+_ALIEN_MISSILE_OFFSET_STATE)
+        JR NZ,.skip
+
+        LD DE,_ALIEN_MISSILE_num
+        LD HL,_current_alien_missile_ptr
+        LD (HL),DE
+        
+        CALL blank
+
+        LD (IX+_ALIEN_MISSILE_OFFSET_STATE),_ALIEN_MISSILE_STATE_NOT_ACTIVE_VALUE
+        
+        LD A,0x00
+        LD (IX+_ALIEN_MISSILE_OFFSET_RELOAD_STEP_COUNT),A 
+
+.skip
+
+    ENDM
+
 
 event_alien_missile_hit_player_begin:
     PUSH AF,DE,HL,IX
@@ -68,54 +90,10 @@ event_alien_missile_hit_player_begin:
     LD A,utils.FALSE_VALUE
     LD (_enabled),A
 
-    LD IX,_ALIEN_MISSILE_0
-    BIT _ALIEN_MISSILE_STATE_NOT_ACTIVE_BIT,(IX+_ALIEN_MISSILE_OFFSET_STATE)
-    JR NZ,.skip_0
-
-    LD DE,_ALIEN_MISSILE_0
-    LD HL,_current_alien_missile_ptr
-    LD (HL),DE
+    REMOVE_ALIENT_MISSILE 0
+    REMOVE_ALIENT_MISSILE 1
+    REMOVE_ALIENT_MISSILE 2
     
-    CALL blank
-
-    LD (IX+_ALIEN_MISSILE_OFFSET_STATE),_ALIEN_MISSILE_STATE_NOT_ACTIVE_VALUE
-    
-    LD A,0x00
-    LD (IX+_ALIEN_MISSILE_OFFSET_RELOAD_STEP_COUNT),A 
-
-.skip_0
-    LD IX,_ALIEN_MISSILE_1
-    BIT _ALIEN_MISSILE_STATE_NOT_ACTIVE_BIT,(IX+_ALIEN_MISSILE_OFFSET_STATE)
-    JR NZ,.skip_1
-
-    LD DE,_ALIEN_MISSILE_1
-    LD HL,_current_alien_missile_ptr
-    LD (HL),DE
-    
-    CALL blank
-
-    LD (IX+_ALIEN_MISSILE_OFFSET_STATE),_ALIEN_MISSILE_STATE_NOT_ACTIVE_VALUE
-    
-    LD A,0x00
-    LD (IX+_ALIEN_MISSILE_OFFSET_RELOAD_STEP_COUNT),A 
-
-.skip_1
-    LD IX,_ALIEN_MISSILE_2
-    BIT _ALIEN_MISSILE_STATE_NOT_ACTIVE_BIT,(IX+_ALIEN_MISSILE_OFFSET_STATE)
-    JR NZ,.skip_2
-
-    LD DE,_ALIEN_MISSILE_2
-    LD HL,_current_alien_missile_ptr
-    LD (HL),DE
-    
-    CALL blank
-
-    LD (IX+_ALIEN_MISSILE_OFFSET_STATE),_ALIEN_MISSILE_STATE_NOT_ACTIVE_VALUE
-    
-    LD A,0x00
-    LD (IX+_ALIEN_MISSILE_OFFSET_RELOAD_STEP_COUNT),A 
-
-.skip_2
     POP IX,HL,DE,AF
 
     RET
