309cd49c17bf7bee53bbb1e7037015162c5a33b8
Paul Harvey <paul@pauls-place.me.uk>
2024-06-07 17:06:12 +0100

    Fixed test splash screen

--- game/tests/test_splash_screen.asm
+++ game/tests/test_splash_screen.asm
@@ -1,4 +1,5 @@
 test_splash_screen:
+    CALL splash_screen.setup_interrupt_handler
     CALL splash_screen.draw_splash_screen
     CALL splash_screen.draw_controls_screen
     CALL draw_utils.wipe_screen
