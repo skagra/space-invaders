b8848b45c9c86c1834af16e22f63fcea62c5eb38
Paul Harvey <paul@pauls-place.me.uk>
2024-06-09 09:43:45 +0100

    Added game over and player died delays

---
+++ game/utils/delay.asm
@@ -0,0 +1,34 @@
+delay:
+    
+.PARAM_LEN:    EQU 8
+
+    PUSH BC,DE,IX
+
+    LD  IX,0                                          
+    ADD IX,SP
+
+    LD B,(IX+.PARAM_LEN)
+
+.outer_loop:
+    LD D,0x08
+
+.mid_loop:
+    LD E,0xFF
+
+.inner_loop:
+    NOP
+    NOP
+    NOP
+    NOP
+    
+    DEC E
+    JR NZ,.inner_loop
+
+    DEC D
+    JR NZ,.mid_loop
+
+    DJNZ .outer_loop
+
+    POP IX,DE,BC
+
+    RET
