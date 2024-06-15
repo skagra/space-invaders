a21497ba69ef804063e9b3ec4a37fd70c49a86ae
Paul Harvey <paul@pauls-place.me.uk>
2024-06-15 08:42:21 +0100

    Slightly updated demo scripts

--- game/demos/state.asm
+++ game/demos/state.asm
@@ -4,10 +4,10 @@ _LK:                                EQU keyboard.LEFT_KEY_DOWN_MASK
 _FK:                                EQU keyboard.FIRE_KEY_DOWN_MASK
 
 ; Demo scripts - initial 0,x are delays to allow the animation loop to be established before the script starts
-DEMO_SCRIPT_0:                      BYTE 0,30,_RK,24,_FK,30,_RK,100,_LK|_FK,1,_LK,30,_LK|_FK,1
+DEMO_SCRIPT_0:                      BYTE 0,30,_RK,24,_FK,30,_RK,100,_LK|_FK,20,_LK,60,_LK|_FK,5,_FK,200,0,120
 DEMO_SCRIPT_0_NUMBER_OF_STEPS:      EQU ($-DEMO_SCRIPT_0)/2
 
-DEMO_SCRIPT_1:                      BYTE 0,30,_RK,10,_FK,1,_FK,1,_RK,100,_LK|_FK,1,_RK,50,_FK,1,_LK,100,_FK,1,_RK,20,_FK,1,_FK,1
+DEMO_SCRIPT_1:                      BYTE 0,30,_RK,10,_FK,1,_RK,100,_LK|_FK,1,_RK,40,_FK,1,_LK,70,_FK,1,_RK,20,_FK,1,60,_LK|_FK,5,_FK,200,_RK|_FK,200,_FK,200,_FK,120,_FK,60,0,60
 DEMO_SCRIPT_1_NUMBER_OF_STEPS:      EQU ($-DEMO_SCRIPT_1)/2
 
 ; Struct offsets into demo scripts
